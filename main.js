ebbc894360ba543f0f94eca47c4f6032d2b12dd1
SamuelALSN <samuelgwfirst@gmail.com>
2020-07-03 20:30:18 +0000

    Event handling in vue.js

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-var app = new Vue ({
-    el:'#app',
+var app = new Vue({
+    el: '#app',
     data: {
-        product:'Stocks'
+        product: 'test',
+        image: './assets/vmSocks-green-onWhite.jpg',
+        link: 'www.google.com',
+        inventory: 100,
+        onSale: false,
+        details: ["80% coton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: './assets/vmSocks-green-onWhite.jpg'
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: './assets/vmSocks-blue-onWhite.jpg'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1
+        },
+        updateProduct: function (variantImage) {
+            this.image = variantImage
+        }
     }
 })
